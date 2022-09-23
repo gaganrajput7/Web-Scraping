9c7b9c5327d0bf200e5168c6596e0a00c0fd5f26
gaganrajput7 <gaganrajputbapora@gmail.com>
2022-09-23 09:57:01 +0530

    commit

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-
 const puppeteer = require("puppeteer");
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const fs = require("fs").promises;
 (async () => {
-    try {
-        const browser = await puppeteer.launch({ headless: false }); // headless is a boolean that determines if the browser is visible or not
-        const page = await browser.newPage(); // newPage is a function that creates a new page
-        
-        await page.setViewport({ width: 1200, height: 1000 });
-        await page.setDefaultNavigationTimeout(0);
-        
-        let res = await page.goto(
-            `https://food.grab.com/sg/en/restaurants`,
-            {
-                waitUntil: "networkidle2",
-            }
-            );
-            const $ = cheerio.load('');
-   
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 1000 });
+    await page.setDefaultNavigationTimeout(0);
+
+    let res = await page.goto(`https://food.grab.com/sg/en`, {
+      waitUntil: "networkidle2",
+    });
+    const $ = await cheerio.load('<class id="ant-input">');
+    $(input.ant - input).text("singapore");
+    $.html();
   } catch (error) {
     console.log(error);
   }
